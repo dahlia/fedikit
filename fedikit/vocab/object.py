999338c470743529930af032e95cbbaa6648b0d9
Hong Minhee <hong@minhee.org>
2024-01-07 20:43:19 +0800

    More properties on Object

--- fedikit/vocab/object.py
+++ fedikit/vocab/object.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+from datetime import datetime
 from typing import Union
 
 from ..model.descriptors import id_property, plural_property, singular_property
@@ -47,6 +48,48 @@ class Object(Entity):
         Uri("https://www.w3.org/ns/activitystreams#attributedTo")
     )
 
+    #: Identifies one or more entities that represent the total population of
+    #: entities for which the object can considered to be relevant.
+    audience: Union["Object", Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#audience")
+    )
+
+    #: Plural accessor for :attr:`audience`.
+    audiences: Sequence[Union["Object", Link]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#audience")
+    )
+
+    #: The content or textual representation of the ``Object`` encoded as
+    #: a JSON string.  By default, the value of content is HTML.
+    #: The :attr:`mediaType` property can be used in the object to indicate
+    #: a different content type.
+    #:
+    #: The content *may* be expressed using multiple language-tagged values.
+    content: str | LanguageString = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#content")
+    )
+
+    #: Plural accessor for :attr:`content`.
+    contents: Sequence[str | LanguageString] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#content")
+    )
+
+    #: Identifies the context within which the object exists or an activity
+    #: was performed.
+    #:
+    #: The notion of "context" used is intentionally vague.  The intended
+    #: function is to serve as a means of grouping objects and activities that
+    #: share a common originating context or purpose. An example could be
+    #: all activities relating to a common project or event.
+    context: Union["Object", Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#context")
+    )
+
+    #: Plural accessor for :attr:`context`.
+    contexts: Sequence[Union["Object", Link]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#context")
+    )
+
     #: A simple, human-readable, plain-text name for the object.
     #: HTML markup *must not* be included.
     #: The name *may* be expressed using multiple language-tagged values.
@@ -57,4 +100,158 @@ class Object(Entity):
     #: Plural accessor for :attr:`name`.
     names: Sequence[str | LanguageString] = plural_property(
         Uri("https://www.w3.org/ns/activitystreams#name")
+    )
+
+    #: The date and time describing the actual or expected ending time of
+    #: the object.  When used with an :class:`Activity` object, for instance,
+    #: the :attr:`end_time` property specifies the moment the activity
+    #: concluded or is expected to conclude.
+    end_time: datetime = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#endTime")
+    )
+
+    #: Identifies the entity (e.g. an application) that generated the object.
+    generator: Union["Object", Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#generator")
+    )
+
+    #: Plural accessor for :attr:`generator`.
+    generators: Sequence[Union["Object", Link]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#generator")
+    )
+
+    #: Indicates one or more entities for which this object is considered
+    #: a response.
+    in_reply_to: Union["Object", Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#inReplyTo")
+    )
+
+    #: Plural accessor for :attr:`in_reply_to`.
+    in_reply_tos: Sequence[Union["Object", Link]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#inReplyTo")
+    )
+
+    #: Indicates one or more physical or logical locations associated with
+    #: the object.
+    location: Union["Object", Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#location")
+    )
+
+    #: Plural accessor for :attr:`location`.
+    locations: Sequence[Union["Object", Link]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#location")
+    )
+
+    #: Identifies an entity that provides a preview of this object.
+    preview: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#preview")
+    )
+
+    #: Plural accessor for :attr:`preview`.
+    previews: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#preview")
+    )
+
+    #: The date and time at which the object was published.
+    published: datetime = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#published")
+    )
+
+    #: The date and time describing the actual or expected starting time of
+    #: the object.  When used with an :class:`Activity` object, for instance,
+    #: the :attr:`start_time` property specifies the moment the activity
+    #: began or is scheduled to begin.
+    start_time: datetime = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#startTime")
+    )
+
+    #: A natural language summarization of the object encoded as HTML.
+    #: Multiple language tagged summaries *may* be provided.
+    summary: str | LanguageString = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#summary")
+    )
+
+    #: Plural accessor for :attr:`summary`.
+    summaries: Sequence[str | LanguageString] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#summary")
+    )
+
+    #: One or more "tags" that have been associated with an objects.
+    #: A tag can be any kind of :class:`Object`.  The key difference between
+    #: :attr:`attachment` and :attr:`tag` is that the former implies
+    #: association by inclusion, while the latter implies associated
+    #: by reference.
+    tag: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#tag")
+    )
+
+    #: Plural accessor for :attr:`tag`.
+    tags: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#tag")
+    )
+
+    #: The date and time at which the object was updated.
+    updated: datetime = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#updated")
+    )
+
+    #: Identifies one or more links to representations of the object.
+    url: Uri | Link = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#url")
+    )
+
+    #: Plural accessor for :attr:`url`.
+    urls: Sequence[Uri | Link] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#url")
+    )
+
+    #: Identifies an entity considered to be part of the public primary
+    #: audience of an :class:`Object`.
+    to: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#to")
+    )
+
+    #: Plural accessor for :attr:`to`.
+    tos: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#to")
+    )
+
+    #: Identifies an entity considered to be part of the public primary
+    #: audience of an :class:`Object`.
+    bto: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#bto")
+    )
+
+    #: Plural accessor for :attr:`bto`.
+    btos: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#bto")
+    )
+
+    #: Identifies an :class:`Object` that is part of the public secondary
+    #: audience of this :class:`Object`.
+    cc: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#cc")
+    )
+
+    #: Plural accessor for :attr:`cc`.
+    ccs: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#cc")
+    )
+
+    #: Singular accessor for :attr:`bccs`.
+    bcc: Union["Link", "Object"] = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#bcc")
+    )
+
+    #: Identifies one or more :class:`Object`\ s that are part of the private
+    #: secondary audience of this :class:`Object`.
+    bccs: Sequence[Union["Link", "Object"]] = plural_property(
+        Uri("https://www.w3.org/ns/activitystreams#bcc")
+    )
+
+    #: Identifies the MIME media type of the value of the :attr:`content`
+    #: property.  If not specified, the :attr:`content` property is assumed to
+    #: contain ``text/html`` content.
+    mediaType: str = singular_property(
+        Uri("https://www.w3.org/ns/activitystreams#mediaType")
     )
